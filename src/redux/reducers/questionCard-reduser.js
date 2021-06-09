04cd0e76773654f0e12f3e4dd6d7124777c9aa98
oleksandr.khatuntsev <oleksandr.khatuntsev@trinetix.com>
2021-06-09 13:22:15 +0300

    fixed sorting issue

--- src/redux/reducers/questionCard-reduser.js
+++ src/redux/reducers/questionCard-reduser.js
@@ -1,119 +1,122 @@
-const ADD_QUESTION = 'ADD_QUESTION'
-const UPDATE_QUESTION_TITLE = 'UPDATE_QUESTION_TITLE'
-const UPDATE_QUESTION_DESCRIPTION = 'UPDATE_QUESTION_DESCRIPTION'
-const UPDATE_QUESTION_OPTION = 'UPDATE_QUESTION_OPTION'
-const DRAG_HAPPEND = 'DRAG_HAPPEND'
+const ADD_QUESTION = "ADD_QUESTION"
+const UPDATE_QUESTION_TITLE = "UPDATE_QUESTION_TITLE"
+const UPDATE_QUESTION_DESCRIPTION = "UPDATE_QUESTION_DESCRIPTION"
+const UPDATE_QUESTION_OPTION = "UPDATE_QUESTION_OPTION"
+const DRAG_HAPPENED = "DRAG_HAPPENED"
 
 const initialState = {
-    questions: [
-        {
-            id: 1,
-            questionTitle: '',
-            questionDescription: '',
-            questionOption: '',
-        },
-        {
-            id: 2,
-            questionTitle: '',
-            questionDescription: '',
-            questionOption: '',
-        },
-        {
-            id: 3,
-            questionTitle: '',
-            questionDescription: '',
-            questionOption: '',
-        },
-    ],
-    survayId: 2,
-    questionQuantity: 3,
-    isNoValue: true,
-
+  questions: [
+    {
+      id: 1,
+      questionTitle: "",
+      questionDescription: "",
+      questionOption: "",
+    },
+    {
+      id: 2,
+      questionTitle: "",
+      questionDescription: "",
+      questionOption: "",
+    },
+    {
+      id: 3,
+      questionTitle: "",
+      questionDescription: "",
+      questionOption: "",
+    },
+  ],
+  survayId: 2,
+  questionQuantity: 3,
+  isNoValue: true,
 }
 
 const questionCardReducer = (state = initialState, action) => {
-
-    switch (action.type) {
-        case ADD_QUESTION:
-            return {
-                ...state,
-                questions: [
-                    ...state.questions,
-                    {
-                        id: state.questionQuantity + 1,
-                        questionTitle: '',
-                        questionDescription: '',
-                        questionOption: '',
-                    },
-                ],
-                questionQuantity: state.questionQuantity + 1,
-            }
-        case UPDATE_QUESTION_TITLE:
-            return {
-                ...state,
-                questions: state.questions.map(q => {
-                    if (q.id === action.questionId)
-                        return { ...q, questionTitle: action.questionTitle }
-                    return q
-                }),
-            }
-        case UPDATE_QUESTION_DESCRIPTION:
-            return {
-                ...state,
-                questions: state.questions.map(q => {
-                    if (q.id === action.questionId)
-                        return { ...q, questionDescription: action.questionDescription }
-                    return q
-                }),
-            }
-        case UPDATE_QUESTION_OPTION:
-            return {
-                ...state,
-                questions: state.questions.map(q => {
-                    if (q.id === action.questionId)
-                        return { ...q, questionOption: action.questionOption }
-                    return q
-                }),
-            }
-        case DRAG_HAPPEND:
-            const newState = { ...state }
-            if (action.droppableIdStart === action.droppableIdEnd) {
-                const question = newState.questions.splice(action.droppableIndexStart, 1)
-                newState.questions.splice(action.droppableIndexEnd, 0, ...question)
-                return newState
-            }
-            return state
-        default:
-            return state
-    }
+  switch (action.type) {
+    case ADD_QUESTION:
+      return {
+        ...state,
+        questions: [
+          ...state.questions,
+          {
+            id: state.questionQuantity + 1,
+            questionTitle: "",
+            questionDescription: "",
+            questionOption: "",
+          },
+        ],
+        questionQuantity: state.questionQuantity + 1,
+      }
+    case UPDATE_QUESTION_TITLE:
+      return {
+        ...state,
+        questions: state.questions.map(q => {
+          if (q.id === action.questionId)
+            return { ...q, questionTitle: action.questionTitle }
+          return q
+        }),
+      }
+    case UPDATE_QUESTION_DESCRIPTION:
+      return {
+        ...state,
+        questions: state.questions.map(q => {
+          if (q.id === action.questionId)
+            return { ...q, questionDescription: action.questionDescription }
+          return q
+        }),
+      }
+    case UPDATE_QUESTION_OPTION:
+      return {
+        ...state,
+        questions: state.questions.map(q => {
+          if (q.id === action.questionId)
+            return { ...q, questionOption: action.questionOption }
+          return q
+        }),
+      }
+    case DRAG_HAPPENED:
+      if (action.droppableIdStart !== action.droppableIdEnd) return state
+      const newState = { ...state, questions: [...state.questions] }
+      const question = newState.questions.splice(action.droppableIndexStart, 1)
+      newState.questions.splice(action.droppableIndexEnd, 0, ...question)
+      return newState
+    default:
+      return state
+  }
 }
 
 export const addQuestion = () => ({ type: ADD_QUESTION })
 
-export const updateQuestionTitle = (questionId, questionTitle) =>
-({ type: UPDATE_QUESTION_TITLE, questionId, questionTitle })
+export const updateQuestionTitle = (questionId, questionTitle) => ({
+  type: UPDATE_QUESTION_TITLE,
+  questionId,
+  questionTitle,
+})
 
-export const updateQuestionDescription = (questionId, questionDescription) =>
-({ type: UPDATE_QUESTION_DESCRIPTION, questionId, questionDescription })
+export const updateQuestionDescription = (questionId, questionDescription) => ({
+  type: UPDATE_QUESTION_DESCRIPTION,
+  questionId,
+  questionDescription,
+})
 
-export const updateQuestionOption = (questionId, questionOption) =>
-({ type: UPDATE_QUESTION_OPTION, questionId, questionOption })
+export const updateQuestionOption = (questionId, questionOption) => ({
+  type: UPDATE_QUESTION_OPTION,
+  questionId,
+  questionOption,
+})
 
 export const sortTitleCards = (
-    droppableIdStart,
-    droppableIdEnd,
-    droppableIndexStart,
-    droppableIndexEnd,
-    draggableId
-) => (
-        {
-            type: DRAG_HAPPEND,
-            droppableIdStart,
-            droppableIdEnd,
-            droppableIndexStart,
-            droppableIndexEnd,
-            draggableId,
-        }
-    )
+  droppableIdStart,
+  droppableIdEnd,
+  droppableIndexStart,
+  droppableIndexEnd,
+  draggableId
+) => ({
+  type: DRAG_HAPPENED,
+  droppableIdStart,
+  droppableIdEnd,
+  droppableIndexStart,
+  droppableIndexEnd,
+  draggableId,
+})
 
 export default questionCardReducer
